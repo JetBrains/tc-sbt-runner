ff7994c3601ea1671272d5c56cdb8e0aafd492e3
shkate <shkate@gmail.com>
2015-07-08 11:38:38 +0200

    fix TW-41688 Improve the SBT Runner UI

--- tc-sbt-runner-common/src/main/java/jetbrains/buildServer/sbt/SbtRunnerConstants.java
+++ tc-sbt-runner-common/src/main/java/jetbrains/buildServer/sbt/SbtRunnerConstants.java
@@ -3,7 +3,7 @@ package jetbrains.buildServer.sbt;
 public interface SbtRunnerConstants {
 
   String RUNNER_TYPE = "SBT";
-  String RUNNER_DISPLAY_NAME = "Simple Build Tool (Scala)";
+  String RUNNER_DISPLAY_NAME = "Runs SBT builds";
   String RUNNER_DESCRIPTION = RUNNER_DISPLAY_NAME;
 
   String SBT_HOME_PARAM = "sbt.home";
